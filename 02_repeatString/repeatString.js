31e157f1696762d116bb0e5dcaf7774619335245
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-24 14:43:27 +0700

    Solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  const oneString = string;
+  if (num < 0) return "ERROR";
+  if (num === 0) return "";
+  for (let i = 0; i < num - 1; i++) {
+    string += oneString;
+  }
+  return string;
 };
 
 // Do not edit below this line
