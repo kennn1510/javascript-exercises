28d96565108fee0d675acf3bdbe2f35345713577
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-25 12:27:08 +0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (beginningNumber, endingNumber) {
+  let sum = 0;
+  if (
+    beginningNumber < 0 ||
+    endingNumber < 0 ||
+    !Number.isInteger(beginningNumber) ||
+    !Number.isInteger(endingNumber)
+  ) {
+    return "ERROR";
+  }
+  if (beginningNumber < endingNumber) {
+    for (let i = beginningNumber; i <= endingNumber; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = beginningNumber; i >= endingNumber; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
