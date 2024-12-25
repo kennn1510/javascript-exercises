363ce26ccc2611a31455fe96844526dc72c84447
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-25 17:37:30 +0700

    Add blue h3

--- DOM_Manipulation/javascript.js
+++ DOM_Manipulation/javascript.js
@@ -3,5 +3,14 @@ const container = document.querySelector("#container");
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
 container.appendChild(content);
+
+const redText = document.createElement("p");
+redText.style.color = "red";
+redText.textContent = "Hey I'm red!";
+container.appendChild(redText);
+
+const blueText3 = document.createElement("h3");
+blueText3.style.color = "blue";
+blueText3.textContent = "I'm a blue h3!";
+container.appendChild(blueText3);
