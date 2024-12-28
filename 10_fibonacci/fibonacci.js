dded9982cebbea5f104fac692455e0d101a163bd
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-28 16:32:59 +0700

    Complete my fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  if (num == 1) return 1;
+  else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+  //1,1,2,3,5,8
 };
 
 // Do not edit below this line
