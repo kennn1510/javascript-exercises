e64bedac9a76cedd5c991c1e0bd8f2c18ceb759c
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-28 15:57:19 +0700

    Complete my attempt at 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const newString = string.toLowerCase().replace(/[^\w]/g, "");
+  const newStringMiddleIndex = newString.length / 2;
+  for (let i = 0; i < newStringMiddleIndex; i++) {
+    if (newString[i] !== newString[newString.length - 1 - i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
